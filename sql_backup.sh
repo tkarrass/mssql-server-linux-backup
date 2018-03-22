4e112729a634d5d2f3ac62a0ef2dd59b7ed33502
tkarrass <git@bitsetter.de>
2018-03-22 15:08:04 +0100

    Restrict backups to online databases

--- sql_backup.sh
+++ sql_backup.sh
@@ -4,6 +4,6 @@
 # Not really a bunch of black magic but it works ;)
 #
 
-/opt/mssql-tools/bin/sqlcmd -H localhost -U $BACKUP_USER -P $BACKUP_PASS -Q "set nocount on; select name from sys.databases where not name in ('master', 'tempdb', 'model', 'msdb')" -h -1 | while read dbname; do
+/opt/mssql-tools/bin/sqlcmd -H localhost -U $BACKUP_USER -P $BACKUP_PASS -Q "set nocount on; select name from sys.databases where not name in ('master', 'tempdb', 'model', 'msdb') and state=0" -h -1 | while read dbname; do
 	/opt/mssql-tools/bin/sqlcmd -H localhost -U $BACKUP_USER -P $BACKUP_PASS -Q "BACKUP DATABASE [${dbname}] TO DISK = N'/backup/${dbname}.bak' WITH NOFORMAT, INIT, NAME = N'${dbname}-Full Database Backup', SKIP, NOREWIND, NOUNLOAD, STATS = 10"
 done
